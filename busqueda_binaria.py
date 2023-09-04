6f8715cba0b81f4e45f144bbeb6735c9cf2b3d24
“Nicolas Burgos” <burgosdanielnicolas@gmail.com>
2023-09-04 17:54:56 -0300

    Funcion de Busqueda ingenua y comienzo de funcion de busqueda binaria

--- busqueda_binaria.py
+++ busqueda_binaria.py
@@ -1,3 +1,22 @@
-print("Hola mundo")
+import random
 
-print("Hola mundo")
+import time
+
+# Busqueda Binaria
+
+
+def busqueda_ingenua(lista, objetivo):
+    for i in range(len(lista)):
+        # range(len(lista)) -> 0, 1, 2, 3, 4, ....., len(lista)-1
+        if lista[i] == objetivo:
+            return i
+    return -1
+
+
+def busqueda_binaria(lista, objetivo, limite_inferior=None, limite_superior=None):
+    if limite_inferior is None:
+        limite_inferior = 0 # Inicio de la lista
+    if limite_superior is None:
+        limite_superior = len(lista)-1 # Final de la lista
+    if limite_superior < limite_inferior:
+        return -1
